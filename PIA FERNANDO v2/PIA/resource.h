f2dc10181e0835e93504ad1319f899b80cd35dcd
FFianquetto <125626176+FFianquetto@users.noreply.github.com>
2024-06-07 18:16:54 -0600

    Add files via upload

---
+++ PIA FERNANDO v2/PIA/resource.h
@@ -0,0 +1,118 @@
+﻿//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por PIA.rc
+//
+#define ABORDAR                         3
+#define usuarioVentana                  9
+#define InicioVentana                   101
+#define menuVentana                     103
+#define ventanaPilotos                  107
+#define vueloVentana                    109
+#define ventanaCompra                   113
+#define pasajeroVentana                 114
+#define ventanaCancelacion              115
+#define ventanaAbordaje                 117
+#define ventanaManifiesto               118
+#define INGRESAR                        1001
+#define cuadroUsuario                   1002
+#define cuadroContra                    1003
+#define IDC_EDIT1                       1004
+#define USUARIOS                        1005
+#define PILOTOS                         1006
+#define VUELOS                          1007
+#define PASAJEROS                       1008
+#define COMPRABOLETOS                   1009
+#define fotoAdmin                       1010
+#define RECUERDAME                      1011
+#define ABORDAJE                        1012
+#define LISTAUSUARIOS                   1012
+#define MANIFIESTO                      1013
+#define ADMIN                           1013
+#define SALIR                           1014
+#define USUARIO                         1015
+#define CANCELARBOLETO                  1016
+#define APELLIDOP                       1016
+#define APELLIDOM                       1017
+#define CORREO                          1018
+#define HORAS                           1018
+#define CONTRA                          1019
+#define USUREGISTRO                     1020
+#define RHOMBRE                         1021
+#define ID                              1021
+#define RMUJER                          1022
+#define NACIMIENTO                      1023
+#define REGISTRO                        1024
+#define FOTOUSUARIO                     1025
+#define vueloID                         1025
+#define ADMINFOTO                       1026
+#define asientoID                       1026
+#define NOMBRE                          1027
+#define claveVuelo                      1027
+#define AGREGARFOTO                     1028
+#define NACIONALIDAD                    1028
+#define PATERNO                         1028
+#define REGISTRARUSU                    1029
+#define FOTOUSUARIO2                    1029
+#define MATERNO                         1029
+#define MODIFICARUSU                    1030
+#define REGISTRARPILO                   1030
+#define EDAD                            1030
+#define ELIMINARUSU                     1031
+#define ELIMINARPILO                    1031
+#define NACION                          1031
+#define MODIFICARPILO                   1032
+#define LISTAPILO                       1033
+#define REGISTRAR                       1034
+#define CANCELAR                        1035
+#define ORIGEN                          1036
+#define DESTINO                         1037
+#define FECHAVUELO                      1038
+#define ESTADO                          1038
+#define FECHAREGISTRO                   1039
+#define ASIENTOS                        1040
+#define PILOTO                          1041
+#define MENOR                           1041
+#define RANCHO                          1042
+#define MAYOR                           1042
+#define RESTRECHO                       1043
+#define MENORES                         1043
+#define MENOR2                          1043
+#define NINIOS                          1044
+#define MAYORES                         1044
+#define ADULOTS                         1045
+#define ESTATUS                         1046
+#define LISTAVUELOS                     1048
+#define ADULTOS                         1049
+#define LISTAPASA                       1051
+#define REGISTRARPASA                   1052
+#define MODIFICARPASA                   1053
+#define ELIMINARPASA                    1054
+#define PASAJERO                        1055
+#define VUELO                           1056
+#define GENERACION                      1057
+#define ASIENTO                         1057
+#define RTURISTA                        1058
+#define REJECUTIVO                      1059
+#define PAGO                            1060
+#define COMPRAR                         1061
+#define LISTABOLETOS                    1062
+#define STATUS                          1062
+#define CONFIRMADO                      1063
+#define NOABORDO                        1066
+#define ABORDO                          1067
+#define LISTA2                          1069
+#define APELLIDO                        1070
+#define FECHA                           1071
+#define IDC_BUTTON1                     1072
+#define ARCHIVAR                        1072
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        121
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1073
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
